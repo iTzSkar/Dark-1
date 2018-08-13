46129196a8baafce498ac604498b7e641c56aba8
iTzSkar <38841221+iTzSkar@users.noreply.github.com>
2018-08-13 12:28:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-console.log("Welcome Savage.");
+console.log("Welcome Heno.");
  
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(`**
 قد تكون نهاية اشياء بداية لأشياء أجمل :sparkles:...# 
-https://discord.gg/4S7ugMr **`) 
+https://discord.gg/9rgF48U **`) 
 
 }).catch(console.error)
 })
