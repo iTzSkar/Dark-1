9a7a287c61ed0e550b9691b1d673e0d1af681f8c
DarkSeid <38841221+iTzSkar@users.noreply.github.com>
2018-09-17 16:00:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '.'
 
 client.on('ready', () => {
   console.log('---------------');
   console.log(' Bot Is Online')
   console.log('---------------')
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+ if(message.content.startsWith(prefix + "join")) {
+message.member.voiceChannel.join().catch(e => message.channel.send(e));
+}
 });
 
 client.on('guildMemberAdd', member => {
